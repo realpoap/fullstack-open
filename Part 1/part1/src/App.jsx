ab48c5e474b5bafac91aa54580bd5a9cf68dd1e8
Матьё Роар <alapetite.mathieu@gmail.com>
2024-02-28 07:42:40 -0500

    conditional rendering

--- Part 1/part1/src/App.jsx
+++ Part 1/part1/src/App.jsx
@@ -1,37 +1,49 @@
 import { useState } from 'react'
 
-
-const Display = ({counter}) => <h1>{counter}</h1>
-
-const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
-
 const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
 
-  const [counter, setCounter] = useState(0)
-  console.log('rendering with counter value', counter)
-  const increaseByOne = () => {
-    console.log('increasing, value before', counter)    
-    setCounter(counter + 1)
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    const updateLeft=left+1
+    setLeft(updateLeft)
+    setTotal(updateLeft+right)
   }
 
-  const decreaseByOne = () => { 
-    console.log('decreasing, value before', counter)   
-    setCounter(counter - 1)
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    const updateRight = right+1
+    setRight(updateRight)
+    setTotal(left+updateRight)
   }
 
-  const setToZero = () => {
-    console.log('resetting to zero, value before', counter)    
-    setCounter(0)
-  }
+  const Button = ({ handleClick, text }) => (  
+    <button onClick={handleClick}>{text}</button>
+    )
 
+  const History = (props) => {  
+    if (props.allClicks.length === 0) {    
+      return (      
+      <div>the app is used by pressing the buttons</div>    
+      )  
+    }  
+    return (    
+      <div>button press history: {props.allClicks.join(' ')}</div>  
+    )
+  }
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button onClick={increaseByOne} text='Add'/>
-      <Button onClick={decreaseByOne} text='Substract'/>
-      <Button onClick={setToZero} text='Reset'/>
-    </div>
+      {left}
+      <Button handleClick={handleLeftClick} text='left'/>
+      <Button handleClick={handleRightClick} text='right'/>
+      {right}
+      <History allClicks={allClicks}/>
+      <p>Total: {total}</p>
+      </div>
   )
 }
 
