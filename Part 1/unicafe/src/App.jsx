e62f9a638484e2112fcc9db88acb07920a742108
Матьё Роар <alapetite.mathieu@gmail.com>
2024-02-28 09:40:31 -0500

    1.7 - got that NaN thing to go

--- Part 1/unicafe/src/App.jsx
+++ Part 1/unicafe/src/App.jsx
@@ -4,14 +4,25 @@ const Display = ({text}) => <h1>{text}</h1>
 
 const Button = ({text, onClick}) => <button onClick={onClick}>{text}</button>
 
+const Label = ({text, feedback, unit}) => {
+  const convertedFeedback = feedback ? feedback : 0
+  return (
+    <p>{text}: {convertedFeedback}{unit}</p>
+  )
+}
 
-const Label = ({text, feedback}) => <p>{text}: {feedback}</p>
 
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
+  const sum = (good + neutral + bad)
+
+  // not good that its NaN at the start
+  const avg = (good*1 + neutral*0 + bad*(-1)) / sum
+  const pos = (good/sum)*100
 
   return (
     <div>
@@ -26,6 +37,9 @@ const App = () => {
         <Label text='good' feedback={good}/>
         <Label text='neutral' feedback={neutral}/>
         <Label text='bad' feedback={bad}/>
+        <Label text='total' feedback={sum}/>
+        <Label text='average' feedback={avg}/>
+        <Label text='positive' feedback={pos} unit=' %'/>
       </div>
     </div>
   )
