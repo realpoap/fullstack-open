f673a6aabb0354fa656c3cfffee35f8404633266
Матьё Роар <alapetite.mathieu@gmail.com>
2024-02-29 13:19:43 -0500

    2.8 - numbers, could be prettier

--- Part 2/phonebook/src/App.jsx
+++ Part 2/phonebook/src/App.jsx
@@ -2,33 +2,37 @@ import { useState } from 'react'
 
 const Contacts = ({persons}) => {
   return (
-    persons.map((person) => <li key={person.name}>{person.name}</li>)
+    persons.map((person) => <li key={person.name}>{person.name} : {person.number}</li>)
   )
 }
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', 
+      number: 5419653423
+    }
   ]) 
   const [newName, setNewName] = useState('test')
+  const [newNumber, setNewNumber]  = useState('(1)')
 
-  const handleNameChange = (e) => {
-    setNewName(e.target.value)
-  }
+  const handleNameChange = (e) => setNewName(e.target.value)
+  const handleNumberChange = (e) => setNewNumber(e.target.value)
 
   const addPerson = (e) => {
     e.preventDefault()
     console.log(e.target);
-    if (persons.find((person) => person.name === newName) ) {
-      alert(`${newName} already exists`)
+    if (newName === '' || persons.find((person) => person.name === newName) ) {
+      alert(`${newName} is invalid `)
     }
     else {
       const nameObject = {
         name: newName,
+        number: newNumber,
       }
       // chose to keep the reset outside so we can make changes to the newName in case of typo
       setPersons(persons.concat(nameObject))
       setNewName('')
+      setNewNumber('')
     }
   }
   
@@ -42,6 +46,13 @@ const App = () => {
           <input 
             value={newName}
             onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          number: 
+          <input 
+            value={newNumber}
+            onChange={handleNumberChange}
           />
         </div>
         <div>
