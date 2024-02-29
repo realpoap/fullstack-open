b00339351b4578da0be33f7b268a4a00acea677d
Матьё Роар <alapetite.mathieu@gmail.com>
2024-02-29 17:45:55 -0500

    2.11 - useEffect hook

--- Part 2/part2/src/App.jsx
+++ Part 2/part2/src/App.jsx
@@ -1,11 +1,23 @@
-import { useState } from "react"
-
+import { useState, useEffect } from "react"
+import axios from 'axios'
 import Note from "./components/Note"
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote ] = useState('')
   const [showAll, setShowAll] = useState(true)
+
+  //server import
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(res => {
+        console.log('promise fulfilled')
+        setNotes(res.data)
+      })
+  }, [])
+  console.log('render', notes.length, 'notes');
 
   const addNote = (e) => {
     e.preventDefault()
--- Part 2/phonebook/src/App.jsx
+++ Part 2/phonebook/src/App.jsx
@@ -1,19 +1,24 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 import Contacts from './components/Contacts'
 import Form from './components/Form'
 import Search from './components/Search'
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('John Doe')
   const [searchName, setSearchName] = useState('')
   const [newNumber, setNewNumber]  = useState('(1)')
+
+useEffect(() => {
+  axios
+    .get('http://localhost:3001/persons')
+    .then(res => {
+      setPersons(res.data)
+    })
+}, [])
 
   const handleNameChange = (e) => setNewName(e.target.value)
   const handleNumberChange = (e) => setNewNumber(e.target.value)
