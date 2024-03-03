190feab805de06e50c944591c6256e8bc833a3ea
Матьё Роар <alapetite.mathieu@gmail.com>
2024-03-03 00:48:53 -0500

    2.20 - bigger image

--- Part 2/countries/src/components/Weather.jsx
+++ Part 2/countries/src/components/Weather.jsx
@@ -29,7 +29,7 @@ useEffect(() => {
             <h2>Weather</h2>
             <p>Temperature: {weather.main.temp} Celsius</p>
             <p className='weather'>{weather.weather[0].description}</p>
-            <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description}/>
+            <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@4x.png`} alt={weather.weather[0].description}/>
             <p>Wind: {weather.wind.speed}m/s</p>
         </div>
     )
