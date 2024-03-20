8a875325ad00f764ad679009fa7951b34e369b22
Матьё Роар <alapetite.mathieu@gmail.com>
2024-03-20 19:01:39 -0400

    part 3 course mat

--- Part 3/part3/index.js
+++ Part 3/part3/index.js
@@ -10,6 +10,15 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
 
+// ERROR HANDLER
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({error: 'malformed id'})
+  }
+  next(error)
+}
+
 // ROUTES
 
 app.get('/', (request, response) => {
@@ -23,18 +32,37 @@ app.get('/api/notes/', (request, response) => {
       })
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
     Note.findById(request.params.id)
         .then(note => {
-          response.json(note)
+          if (note) {
+            response.json(note)
+          } else {
+            response.status(404).end
+          }
         })
+        .catch(err => next(err))
   })
 
+app.put('/api/notes/:id', (req, res) => {
+  const body = req.body
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  //the optional { new: true } parameter, which will cause our event handler to be called with the new modified document instead of the original
+  Note.findByIdAndUpdate(req.params.id, note, {new: true})
+      .then(updatedNote => {
+        res.json(updatedNote)
+      })
+      .catch(err => next(err))
+})
+
 app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
-  
-    response.status(204).end()
+    Note.findByIdAndDelete(request.params.id)
+        .then(result => response.status(204).end)
+        .catch(error => next(error))
   })  
 
 app.post('/api/notes', (req, res) => {
@@ -57,6 +85,16 @@ app.post('/api/notes', (req, res) => {
       })
 
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
