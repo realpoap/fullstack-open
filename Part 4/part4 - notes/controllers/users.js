d4e24b45808f959bed173b8ba5699ab0ace55890
Матьё Роар <alapetite.mathieu@gmail.com>
2024-04-23 13:24:13 -0400

    4.9 - token auth

--- Part 4/part4 - notes/controllers/users.js
+++ Part 4/part4 - notes/controllers/users.js
@@ -2,28 +2,27 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
-usersRouter.get('/'), async (request, response) => {
-	const users = await User
-		.find({})
-		.populate('notes', { content: 1, important: 1 })
-	response.json(users)
-}
 usersRouter.post('/', async (request, response) => {
-	const { username, name, password } = request.body
+  const { username, name, password } = request.body
 
-	const saltRounds = 10
-	const passwordHash = await bcrypt.hash(password, saltRounds)
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(password, saltRounds)
 
-	const user = new User({
-		username,
-		name,
-		passwordHash,
-	})
+  const user = new User({
+    username,
+    name,
+    passwordHash,
+  })
 
-	const savedUser = await user.save()
+  const savedUser = await user.save()
 
-	response.status(201).json(savedUser)
+  response.status(201).json(savedUser)
 })
 
+usersRouter.get('/', async (request, response) => {
+  const users = await User
+    .find({}).populate('notes', { content: 1, important: 1 })
+  response.json(users)
+})
 
 module.exports = usersRouter
--- Part 4/part4 - notes/tests/test_helper.js
+++ Part 4/part4 - notes/tests/test_helper.js
@@ -1,35 +1,36 @@
 const Note = require('../models/note')
 const User = require('../models/user')
 
-const initialNotes = [{
-	content: 'HTML is easy',
-	important: false,
-}, {
-	content: 'Browser can execute only JavaScript',
-	important: true,
-},]
+const initialNotes = [
+  {
+    content: 'HTML is easy',
+    important: false
+  },
+  {
+    content: 'Browser can execute only JavaScript',
+    important: true
+  }
+]
 
 const nonExistingId = async () => {
-	const note = new Note({ content: 'willremovethissoon' })
-	await note.save()
-	await note.deleteOne()
+  const note = new Note({ content: 'willremovethissoon' })
+  await note.save()
+  await note.deleteOne()
 
-	return note._id.toString()
+  return note._id.toString()
+}
+
+
+const usersInDb = async () => {
+  const users = await User.find({})
+  return users.map(user => user.toJSON())
 }
 
 const notesInDb = async () => {
-	const notes = await Note.find({})
-	return notes.map(note => note.toJSON())
-}
-
-const usersInDb = async () => {
-	const users = await User.find({})
-	return users.map(u => u.toJSON())
+  const notes = await Note.find({})
+  return notes.map(note => note.toJSON())
 }
 
 module.exports = {
-	initialNotes,
-	nonExistingId,
-	notesInDb,
-	usersInDb,
+  initialNotes, nonExistingId, notesInDb, usersInDb
 }
