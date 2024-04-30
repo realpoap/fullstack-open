a1630d7a3dc1e6861b84f6dc96ce539650b3ed28
Матьё Роар <alapetite.mathieu@gmail.com>
2024-04-30 16:30:08 -0400

    4.18 - token auth

--- Part 4/blog/app.js
+++ Part 4/blog/app.js
@@ -6,6 +6,8 @@ const mongoose = require('mongoose')
 
 const blogRouter = require('./controllers/blog')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
@@ -26,6 +28,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
