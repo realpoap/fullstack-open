8560d9d90a7e8167d5702b98b0f70ba4b05c1faf
Матьё Роар <alapetite.mathieu@gmail.com>
2024-05-04 13:23:17 -0400

    fixing notes creation

--- Part 5/notes/src/App.jsx
+++ Part 5/notes/src/App.jsx
@@ -67,6 +67,7 @@ const App = () => {
       const user = await loginService.login({
         username, password
       })
+      noteService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
