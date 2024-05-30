31cf0969b052359ae4222e11e7fe0a638eaaf4e4
Матьё Роар <alapetite.mathieu@gmail.com>
2024-05-30 08:06:35 -0400

    5.7 - details visibility

--- Part 5/bloglist-frontend-main/src/components/Blog.jsx
+++ Part 5/bloglist-frontend-main/src/components/Blog.jsx
@@ -1,7 +1,38 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+
+  const [detailsVisibility, setDetailsVisibility] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+
+
+  return (
+    <div style={blogStyle}>
+      <div >
+        {blog.title} {blog.author}
+      </div>
+      <button name="view" onClick={() => setDetailsVisibility(!detailsVisibility)}>
+        {detailsVisibility ? 'hide' : 'view'}
+      </button>
+      {detailsVisibility &&
+        <div>
+          <p>Url : {blog.url}</p>
+          <p>Likes : {blog.likes} <button>Like</button></p>
+          <p>User : {blog.user}</p>
+          {/* how do you access the user from the front-end ? */}
+        </div>}
+    </div >
+  )
+
+
+}
 
 export default Blog
