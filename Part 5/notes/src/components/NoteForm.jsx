f8283dd0324469270bd669b46070023b4149cfd9
Матьё Роар <alapetite.mathieu@gmail.com>
2024-05-31 08:34:09 -0400

    5 - propTypes

--- Part 5/notes/src/components/NoteForm.jsx
+++ Part 5/notes/src/components/NoteForm.jsx
@@ -1,33 +1,33 @@
-import { useState } from "react"
+import { useState } from 'react'
 
 const NoteForm = ({ createNote }) => {
 
-	const [newNote, setNewNote] = useState('')
+  const [newNote, setNewNote] = useState('')
 
-	const addNote = (event) => {
-		event.preventDefault()
-		createNote({
-			content: newNote,
-			important: true
-		})
+  const addNote = (event) => {
+    event.preventDefault()
+    createNote({
+      content: newNote,
+      important: true
+    })
 
-		setNewNote('')
-	}
+    setNewNote('')
+  }
 
 
-	return (
-		<div>
-			<h2>Create a new Note</h2>
+  return (
+    <div>
+      <h2>Create a new Note</h2>
 
-			<form onSubmit={addNote}>
-				<input
-					value={newNote}
-					onChange={event => setNewNote(event.target.value)}
-				/>
-				<button type="submit">save</button>
-			</form>
-		</div>
-	)
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={event => setNewNote(event.target.value)}
+        />
+        <button type="submit">save</button>
+      </form>
+    </div>
+  )
 }
 
 export default NoteForm
