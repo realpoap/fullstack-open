7481abc742b77a5c3b7961f29f71c38851bc36fe
Матьё Роар <alapetite.mathieu@gmail.com>
2024-09-19 15:42:39 -0400

    8.2 - allBooks query

--- Part 8/part-8-library/index.js
+++ Part 8/part-8-library/index.js
@@ -100,9 +100,9 @@ const typeDefs = `
   type Book {
     title: String!
 		published: Int!
-		author: Author!
+		author: String!
 		id: ID!
-		genres: [Genre!]!
+		genres: [String!]!
   }
 	type Genre {
 		genre: String!
@@ -111,14 +111,17 @@ const typeDefs = `
 	type Query {
 		bookCount: Int!
 		authorCount: Int!
+		allBooks: [Book!]!
 	}
 `
 
 const resolvers = {
 	Query: {
 		bookCount: () => books.length,
-		authorCount: () => authors.length
-
+		authorCount: () => authors.length,
+		allBooks: (root, args) => {
+			return books
+		}
 	}
 }
 
