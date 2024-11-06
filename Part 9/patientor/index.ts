5d32a987df472cab0466629fce443a5ff487e6a8
Матьё Роар <alapetite.mathieu@gmail.com>
2024-11-06 09:33:51 -0500

    9.9 - fixed cors + api/patients

--- Part 9/patientor/index.ts
+++ Part 9/patientor/index.ts
@@ -1,14 +1,23 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+
+
+app.use(cors());
 app.use(express.json());
 
-const PORT = 3000;
+
+const PORT = 3001;
 
 app.get('/api/ping', (_req,res) => {
 	console.log('someone pinged');
 	res.send('pong');
 	
+});
+
+app.get('/api/patients', (_req, res) => {
+	res.send({});
 });
 
 app.listen(PORT, ()=> {
