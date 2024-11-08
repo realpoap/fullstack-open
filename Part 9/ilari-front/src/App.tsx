1382a0869569f1980d19cd67cf2e51dce9ee80c1
Матьё Роар <alapetite.mathieu@gmail.com>
2024-11-08 17:01:10 -0500

    9.20 - radiobutton (needs fixing)

--- Part 9/ilari-front/src/App.tsx
+++ Part 9/ilari-front/src/App.tsx
@@ -1,15 +1,16 @@
 import { useEffect, useState } from "react"
 
-import { DiaryEntry, NewDiaryEntry } from "./types"
+import { DiaryEntry, NewDiaryEntry, Visibility, Weather } from "./types"
 import Diary from "./components/Diary"
 import { getAllDiaries, postDiary } from "./services/diariesService"
 import Alert from "./components/Alert"
 
 function App() {
   const [diaries, setDiaries] = useState<DiaryEntry[]>([])
+  const [newDiary, setNewDiary] = useState<NewDiaryEntry>()
   const [comment, setComment] = useState('')
-  const [visibility, setVisibility] = useState('')
-  const [weather, setWeather] = useState('')
+  const [visibility, setVisibility] = useState<Visibility>()
+  const [weather, setWeather] = useState<Weather>()
   const [date, setDate] = useState('')
   const [message, setMessage] = useState('')
 
@@ -22,17 +23,27 @@ function App() {
 
   const createDiary = (e:React.SyntheticEvent) => {
     e.preventDefault()
-    const objectDiary = {
-      date: date,
-      visibility: visibility,
-      weather: weather,
-      comment: comment
-    }
-    postDiary(objectDiary as NewDiaryEntry)
-      .then(data => {
-        setDiaries(diaries.concat(data));
+    if (date && visibility && weather) {
+      setNewDiary({
+        date: date,
+        visibility: visibility,
+        weather: weather,
+        comment: comment
       })
-      .catch(error => notify(error.message))
+    }
+    if(newDiary !== undefined){
+      postDiary(newDiary)
+        .then(data => {
+            setDiaries(diaries.concat(data));
+            notify('all good')
+        })
+        .catch(error => notify(error.message))
+    }
+    setComment('')
+    setDate('')
+    // FIXME:
+    // setVisibility('')
+    // setWeather('')
   }
 
   const notify = (error:string) => {
@@ -52,45 +63,100 @@ function App() {
       <Alert style={alertStyle} message={message}/>
       <h2>Add new entry :</h2>
       <form onSubmit={createDiary}>
-        <div>
-        <label>Date
-          <input
-            type="date"
-            value={date}
-            onChange={(e)=>setDate(e.target.value)}
-          />
-        </label>
-        </div>
-        <div>
-
-        <label>Visibility
-          <input
-            type='string'
-            value={visibility}
-            onChange={(e)=>setVisibility(e.target.value)}
-          />
-        </label>
-        </div>
-        <div>
-
-        <label>Weather
-          <input
-            type="string"
-            value={weather}
-            onChange={(e)=>setWeather(e.target.value)}
-          />
-        </label>
-        </div>
-        <div>
-
-        <label>Comment
-          <input
-            type="string"
-            value={comment}
-            onChange={(e)=>setComment(e.target.value)}
-          />
-        </label>
-        </div>
+        <>
+          <label>Date
+            <input
+              type="date"
+              value={date}
+              onChange={(e)=>setDate(e.target.value)}
+            />
+          </label>
+          <div id="visibility">
+            Visibility
+            <label>
+              <input
+                type='radio'
+                name="visibility"
+                value='poor'
+                onChange={()=>setVisibility(Visibility.Poor)}
+              />poor
+            </label>
+            <label>
+              <input
+                type='radio'
+                name="visibility"
+                value='ok'
+                onChange={()=>setVisibility(Visibility.Ok)}
+              />ok
+            </label>
+            <label>
+              <input
+                type='radio'
+                name="visibility"
+                value='good'
+                onChange={()=>setVisibility(Visibility.Good)}
+              />good
+            </label>
+            <label>
+              <input
+                type='radio'
+                name="visibility"
+                value='great'
+                onChange={()=>setVisibility(Visibility.Great)}
+              />great
+            </label>
+          </div>
+          <div id="weather">
+            Weather
+            <label>
+              <input
+                type="radio"
+                name="weather"
+                value='sunny'
+                onChange={()=>setWeather(Weather.Sunny)}
+              />sunny
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="weather"
+                value='rainy'
+                onChange={()=>setWeather(Weather.Rainy)}
+              />rainy
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="weather"
+                value='cloudy'
+                onChange={()=>setWeather(Weather.Cloudy)}
+              />cloudy
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="weather"
+                value='stormy'
+                onChange={()=>setWeather(Weather.Stormy)}
+              />stormy
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="weather"
+                value='windy'
+                onChange={()=>setWeather(Weather.Windy)}
+              />windy
+            </label>
+          </div>
+          <label>Comment
+            <input
+              type="string"
+              value={comment}
+              onChange={(e)=>setComment(e.target.value)}
+            />
+          </label>
+        </>
         <button type="submit">Send</button>
       </form>
       <h2>Diary Entries</h2>
